08d782bf8153ccb336fd67d5a42a572dd0a7df56
MaxLord <github@maxlord.ru>
2012-07-23 09:57:12 +0400

    singleton code

--- book_head_first_patterns/5-Singleton/singleton1.php
+++ book_head_first_patterns/5-Singleton/singleton1.php
@@ -4,3 +4,52 @@
  * Date: 7/23/12
  * Time: 8:56 AM
  */
+
+class Singleton
+{
+    protected static $instance;
+
+    /**
+     * Защищаем от создания через new Singleton
+     */
+    private function __construct()
+    {
+
+    }
+
+    /**
+     * Защищаем от создания через clone
+     */
+    private function __clone()
+    {
+
+    }
+
+    /**
+     * Защищаем от создания через wakeup
+     */
+    private function __wakeup()
+    {
+
+    }
+
+    /**
+     * @static
+     * @return Singleton
+     */
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new Singleton();
+        }
+        return (self::$instance);
+    }
+
+    public function doAction()
+    {
+
+    }
+}
+
+// применение
+Singleton::getInstance()->doAction();
