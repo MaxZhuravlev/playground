f622b80e2e32ae57e93af33a5a086da13a75ffb0
MaxLord <github@maxlord.ru>
2012-07-27 17:31:48 +0400

    adapter

---
+++ book_head_first_patterns/7-Adapter/adapter1.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by Max Zhuravlev
+ * Date: 7/25/12
+ * Time: 10:54 AM
+ */
+
+
+class AdapterClient
+{
+    public function __construct()
+    {
+        $adapter = new AdapterAdapter();
+        $adapter->request();
+    }
+}
+
+interface AdapterTargetInterface
+{
+    public function request();
+}
+
+class AdapterAdapter implements AdapterTargetInterface
+{
+    public function request()
+    {
+        $adaptee = new AdapterAdaptee();
+        $adaptee->specificRequest(1);
+    }
+}
+
+class AdapterAdaptee
+{
+    public function specificRequest($param)
+    {
+        echo $param;
+    }
+}
+
+
+$client = new AdapterClient();
